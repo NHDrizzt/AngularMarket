7a2aa4359a24afe8cd7b4372b4f7340109a0bd86
Rokol <biel_s.carvalho2@hotmail.com>
2022-04-13 20:16:55 -0300

    App start

---
+++ backend/mercado/src/test/kotlin/com/online/mercado/MercadoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.online.mercado
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MercadoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
